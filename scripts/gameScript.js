3a5a574c33ebdb9e22defb9f201ea1066a35fecd
Fernando Blanco <fern.alej.blanco@gmail.com>
2019-05-30 09:52:29 -0600

    Added more logic to the button functions

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,8 +1,20 @@
-exports.populateButtons = () => {
 
+exports.pageButton = [];
+exports.clickWorth = 0;
+exports.balance = 0;
+
+exports.populateButtons = () => {
+    let button = document.createElement('button');
+    button.name = 'test button';
+    button.innerHTML = 'This is Test';
+    pageButton[0] = button;
 };
 
 exports.listenForClick = evt => {
+    pageButton[0].addEventListener('click', doSomethingAfterClick(clickWorth, balance));
+};
 
+exports.addClickWorthToBalance = (clickWorth, balance) => {
+    balance += clickWorth;
 };
 
