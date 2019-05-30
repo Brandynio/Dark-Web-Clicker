c9fb71c431792c04a958cdc64d7fcdef5a419c41
Brandyn <bbushong@student.neumont.edu>
2019-05-30 09:50:28 -0600

    started on per second currency

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ let clickerData = {
     counterHackPrice = 12000,
     motionSensorPrice = 130000,
     alarmSystemPrice = 1400000,
-    policeScanner = 20000000
+    policeScannerPrice = 20000000,
+    playerTorBrowsers = 0,
+    playerAdBlockers = 0,
+    playerAntiViruses = 0,
+    playerCounterHacks = 0,
+    playerMotionSensors = 0,
+    playerAlarmSystems = 0,
+    playerPoliceScanners = 0
 }
+
+const currencyLoop = () => {
+    let profit = 0;
+    profit += (clickerData.playerTorBrowsers * 0.1);
+}
+
+let currencyInterval = setInterval(currencyLoop, 1000);
